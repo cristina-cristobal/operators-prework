34748f161a8db119d4175da6f4ea6f758626b60f
Cristina Cristobal <cristina.cristobal@gmail.com>
2018-12-20 16:22:27 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,10 @@
 def unsafe?(speed)
 if speed >= 40 && speed <= 60
-  return "false"
+  return false
 elsif speed > 60
-  return "true"
+  return true
 elsif speed < 40
-  return "true"
+  return true
 end
 end
 
